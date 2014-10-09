64d9c3172287058fecc2a8b5029b2e93c6286d6b
Zifeng Xu <xuzifeng89@gmail.com>
2014-10-09 12:03:05 -0400

    typescripts

--- BoilerPlate/Scripts/index.ts
+++ BoilerPlate/Scripts/index.ts
@@ -1,1 +1,28 @@
-﻿ 
+﻿class Person {
+    name: string;
+    age: number;
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    moves(meters: number) {
+        alert(this.name + "moves" + meters + "m");
+    }
+}
+
+class Teacher extends Person {
+    course: string;
+    constructor(name: string, age: number, course: string) {
+        super(name, age);
+        this.course = course;
+    }
+
+    teachers() {
+        alert(this.name + "teaches" + this.course);
+    }
+}
+
+var tom = new Teacher("Tom", 30, "COMPSCI");
+tom.moves(5);
+tom.teachers();
